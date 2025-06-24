a3575cbab376423945a6f6cd78170524c4fce9bb
Muhammed Cavus <mcavus048@gmail.com>
2025-06-24 17:50:49 +0200

    :sparkles: Suppression d'elements inutile

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,10 +3,6 @@ import { Link, Outlet } from "react-router-dom";
 function App() {
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-      <hr />
       <Outlet />
     </div>
   );
